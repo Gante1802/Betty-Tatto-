ed8bbbccd690f5e5006d742e3f1ffc7b13bfb464
Gante1802 <diegosolergomez95@gmail.com>
2026-06-24 09:25:58 -0400

    server 0.0.2

--- server.js
+++ server.js
@@ -9,4 +9,30 @@ app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(process.env.MONGO_URI);
+
 let db;
+
+//Conectar con MongoDB
+async function connectDB() {
+  await client.connect();
+  db = client.db("Betty_Tatto");
+  console.log("Conectado a la base de datos");
+}
+
+//prueba
+app.get("/", (req, res) => {
+  res.send("backend funcionando");
+});
+
+//crear usuario
+app.post("/users", async (req, res) => {
+  const result = await db.collection("users").insertOne(req.body);
+  res.json(result);
+});
+
+//ejecutar server
+const port = process.env.PORT || 3000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+});
